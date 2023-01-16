996d1aa85eb474a14d8acb214dafb6d3eb81bfb0
noori <rajilabana@gmail.com>
2023-01-16 09:42:10 +0000

    add question to questions js

---
+++ assets/js/logic.js
@@ -0,0 +1,14 @@
+// things we need
+// it's a quiz challenge
+// need starts screen with start button
+// when starts button is clicked we starts the timer and show the question
+// add eventlistener to the start button
+// need questions
+// need choices for each question
+// if question is right move to the next question
+// if question is wrong substract from timer
+// need a timer
+// we also need highscores 
+//when its done it prompts to user to put initials
+
+
---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        questionName: "what does js stands for?",
+        choices: ["just saying", "java Script", "just smile", "jono script"],
+        correct: "java Script"
+
+        
+    }, 
+    {
+        questionName: "what does CSS stands for?",
+        choices: ["Cascading style sheets", "Common style sheets", "", "Current Style Sheets", "Comman Standard Style"],
+        correct: "cascading style sheets"
+
+        
+    }, 
+     {
+        questionName: "what does HTML stands for?",
+        choices: ["Hypertext Markup Language", "highly mantained language ", "Higher Markup Language", "Hello My language"],
+        correct: "java Script"
+
+        
+    }, 
+     {
+        questionName: "who invented Java Script?",
+        choices: ["Douglas Crockford", "sheryl Sandberg", "Brendon Eich", "jono script"],
+        correct: "Brendan Eich"
+
+        
+    }, 
+    
+]
